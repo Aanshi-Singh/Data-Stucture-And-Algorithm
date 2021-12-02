aaebb459f93435018b580e898e1ee4772e784f21
Aanshi <aanshisingh04022002@gmail.com>
2021-12-02 13:22:39 +0530

    initial commit

---
+++ CP/reverseArray.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int lengthOfArray;
+    cin>>lengthOfArray;
+    int arr[lengthOfArray];
+    for(int i = 0 ; i<lengthOfArray; i++){
+        cin>>arr[i];
+    }
+}
