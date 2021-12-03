d0c53b6fa938e38101e97c5951ca34e75dc454a1
Aanshi <aanshisingh04022002@gmail.com>
2021-12-04 01:40:42 +0530

    reverse array

--- CP/reverseArray.cpp
+++ CP/reverseArray.cpp
@@ -7,4 +7,15 @@ int main(){
     for(int i = 0 ; i<lengthOfArray; i++){
         cin>>arr[i];
     }
+    
+    for(int i = 0; i< lengthOfArray/2; i++){
+        int temp = arr[i];
+        arr[i] = arr[lengthOfArray-i-1];
+        arr[lengthOfArray-i-1] = temp;
+    }
+    for(int i = 0 ; i<lengthOfArray; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
 }
